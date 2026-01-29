797b8de6eda51cded5c460a3134b0f694d751880
Cuneyt Yildirim <yildirim.cuneyt.it@gmail.com>
2026-01-29 01:04:57 -0500

    updating code base based on requirement

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,135 @@
 
-import java.sql.DriverManager;
+import java.io.Console;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.io.Console;
-import java.sql.Connection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
   public static void main(String[] args) {
     var console = System.console();
-    var user = "root";// new String(console.readLine("Username: "));
-    var pass = "12345678";// new String(console.readLine("Password: "));
-    List<Student> students;
-    List<EnrollmentStudent> enrollmentStudents;
 
-    String url = "jdbc:mysql://localhost:3306/school_mgmt?useSSL=false&serverTimezone=UTC";
+    // Reading each file differently then matching student who enrolled a class from the their
+    // related list
+    List<Student> students = readFromFileGeneric("student.txt",
+        parts -> new Student(Integer.parseInt(parts[0]), parts[1], parts[2]));
 
-    try (var conn = DriverManager.getConnection(url, user, pass)) {
-      if (!conn.isClosed()) {
-        System.out.println("Connected");
+    List<Enrolled> enrolledList = readFromFileGeneric("enrolled.txt",
+        parts -> new Enrolled(Integer.parseInt(parts[0]), Integer.parseInt(parts[1])));
+
+    List<EnrollmentStudent> enrollments = new ArrayList<EnrollmentStudent>();
+
+
+    // Reading file and matching their result from the HashMap
+    Map<Integer, Enrolled> enrolledMap = readFromFileGenericHashMap("enrolled.txt",
+        parts -> new Enrolled(Integer.parseInt(parts[0]), Integer.parseInt(parts[1])),
+        s -> s.getsId());
+
+    Map<Integer, Student> studentMap = readFromFileGenericHashMap("student.txt",
+        parts -> new Student(Integer.parseInt(parts[0]), parts[1], parts[2]), s -> s.getsId());
+
+
+    List<EnrollmentStudent> enrolledStudentList = enrolledMap.values().stream().map(enrollment -> {
+
+      Student student = studentMap.get(enrollment.getsId());
+      if (student != null) {
+        return new EnrollmentStudent(enrollment.getcId(), student.getName());
       }
+      return null;
+    }).filter(Objects::nonNull).collect(Collectors.toList());
 
-      students = getStudents(conn);
-      displayStudents(students);
+    displayEnrollmentStudents(enrolledStudentList);
 
-      System.out.println(" ");
-      enrollmentStudents = getEnrolledStudents(conn);
-      displayEnrollmentStudents(enrollmentStudents);
-      System.out.println(" ");
+    // displayEnrollmentStudents(enrollmentStudents);
+    // var user = "root";// new String(console.readLine("Username: "));
+    // var pass = "12345678";// new String(console.readLine("Password: "));
+    // List<Student> students;
+    // List<EnrollmentStudent> enrollmentStudents;
+    //
+    // String url = "jdbc:mysql://localhost:3306/school_mgmt?useSSL=false&serverTimezone=UTC";
+    //
+    // try (var conn = DriverManager.getConnection(url, user, pass)) {
+    // if (!conn.isClosed()) {
+    // System.out.println("Connected");
+    // }
+    //
+    // students = getStudents(conn);
+    // displayStudents(students);
+    //
+    // System.out.println(" ");
+    // enrollmentStudents = getEnrolledStudents(conn);
+    // displayEnrollmentStudents(enrollmentStudents);
+    // System.out.println(" ");
+    //
+    // } catch (Exception e) {
+    // System.out.println(e);
+    // }
 
-    } catch (Exception e) {
-      System.out.println(e);
+  }
+
+  public static List<EnrollmentStudent> readFromFile(List<Student> students) {
+    List<EnrollmentStudent> enrollmentStudents = new ArrayList<>();
+    try (Scanner sc = new Scanner(new File("enrolled.txt"))) {
+      while (sc.hasNextLine()) {
+        String[] parts = sc.nextLine().split(",");
+        int enrolledId = Integer.parseInt(parts[0]);
+        int sId = Integer.parseInt(parts[1]);
+
+        Student result = students.stream().filter(s -> s.getsId() == sId).findFirst().orElse(null);
+
+        if (result != null) {
+          enrollmentStudents.add(new EnrollmentStudent(enrolledId, result.getName()));
+        }
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("Error: student.csv not found.");
     }
+    return enrollmentStudents;
+  }
 
+  public static <K, T> Map<K, T> readFromFileGenericHashMap(String filename,
+      Function<String[], T> mapper, Function<T, K> idExtractor) {
+    Map<K, T> tMap = new HashMap<>();
+    try (Scanner sc = new Scanner(new File(filename))) {
+      while (sc.hasNextLine()) {
+        String line = sc.nextLine();
+        if (line.trim().isEmpty())
+          continue;
+        String[] parts = line.split(",");
+        T item = mapper.apply(parts);
+        if (item != null) {
+          K key = idExtractor.apply(item);
+          tMap.put(key, item);
+        }
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("Error: student.csv not found.");
+    }
+    return tMap;
+  }
+
+  public static <T> List<T> readFromFileGeneric(String filename, Function<String[], T> mapper) {
+    List<T> list = new ArrayList<>();
+    try (Scanner sc = new Scanner(new File("student.csv"))) {
+      while (sc.hasNextLine()) {
+        String[] parts = sc.nextLine().split(",");
+        T item = mapper.apply(parts);
+        if (item != null) {
+          list.add(item);
+        }
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("Error: student.csv not found.");
+    }
+    return list;
   }
 
   public static List<Student> getStudents(Connection conn) throws SQLException {
